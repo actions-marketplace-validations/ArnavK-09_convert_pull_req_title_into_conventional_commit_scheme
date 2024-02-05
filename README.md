0398b7ce21b4f030ee0f6b001b92b098fdb92472
Arnav K <arnavkaushal09@gmail.com>
2024-02-05 15:10:49 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# WIP
+# 🌟 convert_pull_req_title_into_conventional_commit_scheme
+
+> [!NOTE]  
+> The [**` convert_pull_req_title_into_conventional_commit_scheme `**](https://github.com/marketplace/actions/convert-pull-request-title-into-conventional-commit-scheme) project is a public **GitHub Action** available in the GitHub Marketplace. By utilizing this action, users can automatically __convert new pull request titles into the conventional commits format__ as defined by [conventionalcommits.org](https://www.conventionalcommits.org/).
+>
+> This project is powered by the **Google Gemini Pro API**, which enables seamless integration and efficient conversion of pull request titles.
+
+## 📐 Setup Guide
+
+> **To set up the `convert_pull_req_title_into_conventional_commit_scheme` action, follow these steps:**
+
+### 1. 🔆**Action Setup**
+- Visit the [GitHub Marketplace page](https://github.com/marketplace/actions/convert-pull-request-title-into-conventional-commit-scheme) for the action.
+- Click on the "Set up a workflow" button.
+- Choose the repository where you want to use the action.
+- Create a new workflow file (e.g., `.github/workflows/convert-pull-request-title.yml`).
+- Add the following code to the workflow file:
+
+```yaml
+name: Convert Pull Req Title Into Conventional Commit Scheme
+
+on:
+pull_request:
+    types:
+    - opened
+
+jobs:
+  convert:
+      runs-on: ubuntu-latest
+      steps:
+      - name: Checkout repository
+          uses: actions/checkout@v2
+  
+      - name: Convert Pull Request Title into Conventional Commit Scheme
+          uses: ArnavK-09/convert_pull_req_title_into_conventional_commit_scheme@v1
+          with:
+          gemini_api_key: ${{ secrets.GEMINI_API_KEY }}
+          owner: ${{ github.repository_owner }}
+          repo: ${{ github.repository }}
+          pr_number: ${{ github.event.pull_request.number }}
+          token: ${{ secrets.GITHUB_TOKEN }}
+          include_emoji: true
+```
+
+
+### 2. 🔅 **Action Inputs**
+
+| Input Name | Description |
+|-------------------|-----------------------------------------------------------------------------|
+| `gemini_api_key` | The API key for accessing the Google Gemini LLM API. |
+| `owner` | The owner of the repository where the action is being used. |
+| `repo` | The name of the repository where the action is being used. |
+| `pr_number` | The pull request number for which the title needs to be converted. |
+| `token` | The GitHub token for authentication and authorization. Use `${{ secrets.GITHUB_TOKEN }}` to access it securely. |
+| `include_emoji` | Set this to `true` if you want to include emojis in starting of converted commit prTitle title.
+
+> [!TIP]
+> #### Example Usage
+> To use this action, create a new pull request with a title that follows the conventional commits format.
+> The action will automatically convert the pull request title into a conventional commit message and add it to the pull request.
+
+## 🎋 Links
+- [GitHub Action: convert_pull_req_title_into_conventional_commit_scheme](https://github.com/marketplace/actions/convert-pull-request-title-into-conventional-commit-scheme)
+- [Repository: ArnavK-09/convert_pull_req_title_into_conventional_commit_scheme](https://github.com/ArnavK-09/convert_pull_req_title_into_conventional_commit_scheme)
+- [Title Format: Conventional Commit Rules](https://www.conventionalcommits.org/)
--- index.js
+++ index.js
@@ -1,20 +1,137 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+// imports
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
+/**
+ * Generate proper prompt for llm
+ * @param {string} title
+ * @param {string} description
+ * @param {boolean} include_emoji
+ * @returns
+ */
+const prompt = (title, description, include_emoji = false) => {
+  return `
+    # Give me conventional commit message following www.conventionalcommits.org for pull request with title and description mentioned above${include_emoji ? ", also include emoji in starting" : ""}. Only in single Line.
+    
+    # Documentation for conventionalcommits.org:
+    The Conventional Commits specification is a lightweight convention on top of commit messages. It provides an easy set of rules for creating an explicit commit history; which makes it easier to write automated tools on top of. This convention dovetails with SemVer, by describing the features, fixes, and breaking changes made in commit messages.
+    
+    ### The commit message should be structured as follows:
+    
+    <type>[optional scope]: <description>
+    
+    [optional body]
+    
+    [optional footer(s)]
 
+    ### The commit contains the following structural elements, to communicate intent to the consumers of your library:
+    
+    fix: a commit of the type fix patches a bug in your codebase (this correlates with PATCH in Semantic Versioning).
+    feat: a commit of the type feat introduces a new feature to the codebase (this correlates with MINOR in Semantic Versioning).
+    BREAKING CHANGE: a commit that has a footer BREAKING CHANGE:, or appends a ! after the type/scope, introduces a breaking API change (correlating with MAJOR in Semantic Versioning). A BREAKING CHANGE can be part of commits of any type.
+    types other than fix: and feat: are allowed, for example @commitlint/config-conventional (based on the Angular convention) recommends build:, chore:, ci:, docs:, style:, refactor:, perf:, test:, and others.
+    footers other than BREAKING CHANGE: <description> may be provided and follow a convention similar to git trailer format.
+    Additional types are not mandated by the Conventional Commits specification, and have no implicit effect in Semantic Versioning (unless they include a BREAKING CHANGE). A scope may be provided to a commit's type, to provide additional contextual information and is contained within parenthesis, e.g., feat(parser): add ability to parse arrays.
+    
+    # Pull request data:
+    Pull Request Title: ${title}
+    Pull Request Description: ${description}
+    `;
+};
 
-try {
-  // Fetch the value of the input 'who-to-greet' specified in action.yml
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  
-  // Record the time of greeting as an output
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+/**
+ * Logic For Workflow
+ */
+const initAction = async () => {
+  try {
+    /**
+     * Fetching inputs for actions
+     **/
+    const GEMINI_API_KEY = core.getInput("gemini_api_key", { required: true });
+    const owner = core.getInput("owner", { required: true });
+    const repo = core.getInput("repo", { required: true });
+    const pull_number = core.getInput("pr_number", { required: true });
+    const token = core.getInput("token", { required: true });
+    const includeEmoji = core.getBooleanInput("include_emoji", {
+      required: false,
+    });
 
+    /**
+     * Inform user about emoji
+     */
+    if (!includeEmoji) {
+      core.notice("Emoji shouldn't be included");
+    }
 
+    /**
+     * Now we need to create an instance of Octokit which will use to call
+     * GitHub's REST API endpoints.
+     * We will pass the token as an argument to the constructor. This token will be used to authenticate our requests.
+     **/
+    const octokit = new github.getOctokit(token);
+    core.debug("Created github client");
 
-} catch (error) {
-  // Handle errors and indicate failure
-  core.setFailed(error.message);
-}
+    /**
+     * Fetching data of pull request
+     */
+    const { data: pullReq } = await octokit.request(
+      `GET /repos/${owner}/${repo}/pulls/${pull_number}`,
+      {
+        owner,
+        repo,
+        pull_number,
+      },
+    );
+
+    /**
+     * Creating vars
+     */
+    const prTitle = pullReq.title ?? "No Title";
+    const prDescription = pullReq.description ?? "No Description";
+    core.debug("Fetched Pull request information");
+
+    /**
+     * Creating new Gemini API Client Instance
+     * Also, new gemini-pro model
+     */
+    const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+    const gemini = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    /**
+     * Generating title with gemini
+     */
+    const result = await gemini.generateContent(
+      prompt(prTitle, prDescription, includeEmoji),
+    );
+
+    /**
+     * Manipulating response into string
+     */
+    const response = await result.response;
+    core.debug("Fetched Gemini Response");
+    const gemini_response = response.text();
+
+    /**
+     * Patching pull request with new title
+     */
+    await octokit.request(
+      `PATCH /repos/${owner}/${repo}/pulls/${pull_number}`,
+      {
+        owner,
+        repo,
+        pull_number,
+        title: gemini_response,
+      },
+    );
+    core.debug("Patched pull request with new title");
+  } catch (error) {
+    /**
+     * If any error, inform workflow
+     */
+    core.setFailed(error.message);
+  }
+};
+
+// Call the action
+initAction();
